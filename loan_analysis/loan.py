94dfd6437b38353b9afbf6ab5b731a649fc83034
mahikumar94 <maheshkumar.sh94@gmail.com>
2023-03-02 19:22:35 +0530

    fail test case

--- loan_analysis/loan.py
+++ loan_analysis/loan.py
@@ -1,12 +1,19 @@
 from pyspark.sql import DataFrame, functions as F
+from pyspark.sql import SparkSession
 
 def loanreport():
     spark = SparkSession.builder.master("local[*]").config("spark.port.maxRetries", "1000").config("spark.sql.shuffle.partitions", "1").getOrCreate()
-    loan_df=spark.read.option("header",True).cav("../data/loan.csv")
-    print(f"LOAN::{loan_df}")
-    loan_df.createOrReplace("loan")
-    cust_df=spark.read.option("header",True).csv("../data/customer.csv")
-    cust_df.createOrReplace("cust")
-    print(f"CUST::{cust_df}")
-    df=spark.sql("select * from loan join cust on loan.customer_id = cust.customer_id")
+    loan_df=spark.read.option("header",True).csv("../pythondemo/data/loan.csv")
     
+    loan_df.createOrReplaceTempView("loan")
+    loan_df.show()
+    cust_df=spark.read.option("header",True).csv("../pythondemo/data/customer.csv")
+    cust_df.createOrReplaceTempView("cust")
+    cust_df.show()
+    df=spark.sql("select * from loan join cust on loan.customer_id = cust.customer_id")
+    print(f"count :: {df.count()}")
+    df.show()
+
+
+if __name__ == "__main__":
+    loanreport()
